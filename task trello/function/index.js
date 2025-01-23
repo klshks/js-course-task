688dc152d74361216db16784478330500e3ea6e7
klshks <lyoshakyliushyk@gmail.com>
2025-01-23 12:45:53 +0200

    added task function and day seven

---
+++ task trello/function/index.js
@@ -0,0 +1,11 @@
+function getSum (a, b) {
+    return a + b;
+}
+
+function getUpperCase (string) {
+    return string.toUpperCase();
+}
+
+function squareNumbers (arr) {
+    return arr.map(num => num * num);
+}
